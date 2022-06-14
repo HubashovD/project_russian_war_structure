4ca3f391e33df17ade20f42cba395497ebe19366
Vlad Herasymenko <drimacus182@gmail.com>
2022-06-14 19:27:05 +0300

    path to data

--- js/map.js
+++ js/map.js
@@ -41,7 +41,7 @@ function mapPainter() {
 
 
 
-    d3.csv("data/map.csv", function(data) {
+    d3.csv(window.__base_dir__ + "data/map.csv", function(data) {
 
         /*d3.json("data/ukraine.geojson", function(error, json) {
             if (error) console.error(error);;
